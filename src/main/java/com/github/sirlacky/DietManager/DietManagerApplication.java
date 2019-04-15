48081f6959e314b2fe306a6f93218e9deca8ab30
SirLacky <sirlacky@gmail.com>
2019-04-15 16:07:28 +0200

    Created project structure

---
+++ src/main/java/com/github/sirlacky/DietManager/DietManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.sirlacky.DietManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sirlacky/DietManager/DietManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.sirlacky.DietManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DietManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
